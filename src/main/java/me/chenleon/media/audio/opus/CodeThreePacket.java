54d5aa071e87ce2060694ecf7310570379acc751
Chen Liang <slimhigh@gmail.com>
2020-06-09 22:35:09 +0800

    Refactor code 3 packet dump.

--- src/main/java/me/chenleon/media/audio/opus/CodeThreePacket.java
+++ src/main/java/me/chenleon/media/audio/opus/CodeThreePacket.java
@@ -52,35 +52,15 @@ class CodeThreePacket extends OpusPacket {
 
     @Override
     public byte[] dumpToStandardFormat() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(getTocByte());
-        out.write(getFrameCountByte());
-        byte[] padding = new byte[0];
-        if (hasPadding) {
-            int byteCountOf255 = paddingLength / 255;
-            int lastByteValue = paddingLength - byteCountOf255 * 255;
-            byte[] paddingLengthBytes = new byte[byteCountOf255 + 1];
-            Arrays.fill(paddingLengthBytes, 0, byteCountOf255, (byte) 255);
-            paddingLengthBytes[paddingLengthBytes.length - 1] = (byte) lastByteValue;
-            out.writeBytes(paddingLengthBytes);
-            padding = new byte[paddingLength - byteCountOf255];
-        }
-        if (isVbr) {
-            for (int i = 0; i < frames.size() - 1; i++) {
-                out.writeBytes(OpusUtil.frameLengthToBytes(frames.get(i).length));
-            }
-        }
-        for (byte[] frame : frames) {
-            out.writeBytes(frame);
-        }
-        if (hasPadding) {
-            out.writeBytes(padding);
-        }
-        return out.toByteArray();
+        return dump(true);
     }
 
     @Override
     public byte[] dumpToSelfDelimitingFormat() {
+        return dump(false);
+    }
+
+    private byte[] dump(boolean isStandard) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         out.write(getTocByte());
@@ -96,11 +76,14 @@ class CodeThreePacket extends OpusPacket {
             padding = new byte[paddingLength - byteCountOf255];
         }
         if (isVbr) {
-            for (byte[] frame : frames) {
-                out.writeBytes(OpusUtil.frameLengthToBytes(frame.length));
+            int count = isStandard ? frames.size() - 1 : frames.size();
+            for (int i = 0; i < count; i++) {
+                out.writeBytes(OpusUtil.frameLengthToBytes(frames.get(i).length));
             }
         } else {
-            out.writeBytes(OpusUtil.frameLengthToBytes(frames.get(0).length));
+            if (!isStandard) {
+                out.writeBytes(OpusUtil.frameLengthToBytes(frames.get(0).length));
+            }
         }
         for (byte[] frame : frames) {
             out.writeBytes(frame);
